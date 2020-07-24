7e2d6ef2e143af428dd7e674abacfce74177f472
elshanx <elshanxakbarov@gmail.com>
2020-07-24 22:21:30 +0400

    .

--- src/components/landing/Contact/ContactForm/index.jsx
+++ src/components/landing/Contact/ContactForm/index.jsx
@@ -1,104 +1,26 @@
-import React from 'react';
-import axios from 'axios';
-import { Formik, Form, FastField, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
+import React, { useState } from 'react';
 import { Button, Input } from 'components/common';
 import { Error, Center, InputField } from './styles';
 
 export default () => (
-  <Formik
-    initialValues={{
-      name: '',
-      email: '',
-      message: '',
-      success: false,
-    }}
-    validationSchema={Yup.object().shape({
-      name: Yup.string().required('Full name field is required'),
-      email: Yup.string()
-        .email('Invalid email')
-        .required('Email field is required'),
-      message: Yup.string().required('Message field is required'),
-    })}
-    onSubmit={async ({ name, email, message }, { setSubmitting, resetForm, setFieldValue }) => {
-      try {
-        await axios({
-          method: 'POST',
-          url: `${process.env.GATSBY_PORTFOLIO_FORMIK_ENDPOINT}`,
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          data: JSON.stringify({
-            name,
-            email,
-            message,
-          }),
-        });
-        setSubmitting(false);
-        setFieldValue('success', true);
-        setTimeout(() => resetForm(), 6000);
-      } catch (err) {
-        setSubmitting(false);
-        setFieldValue('success', false);
-				alert('Something went wrong, please try again!') // eslint-disable-line
-      }
-    }}
-  >
-    {({ values, touched, errors, isSubmitting }) => (
-      <Form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-        <InputField>
-          <input type="hidden" name="form-name" value="contact" />
-          <Input
-            as={FastField}
-            type="text"
-            name="name"
-            component="input"
-            aria-label="name"
-            placeholder="Full name*"
-            error={touched.name && errors.name}
-          />
-          <ErrorMessage component={Error} name="name" />
-        </InputField>
-        <InputField>
-          <Input
-            id="email"
-            aria-label="email"
-            component="input"
-            as={FastField}
-            type="email"
-            name="email"
-            placeholder="Email*"
-            error={touched.email && errors.email}
-          />
-          <ErrorMessage component={Error} name="email" />
-        </InputField>
-        <InputField>
-          <Input
-            as={FastField}
-            component="textarea"
-            aria-label="message"
-            id="message"
-            rows="8"
-            type="text"
-            name="message"
-            placeholder="Message*"
-            error={touched.message && errors.message}
-          />
-          <ErrorMessage component={Error} name="message" />
-        </InputField>
-        {values.success && (
-          <InputField>
-            <Center>
-              <h4>Your message has been successfully sent, I will get back to you ASAP!</h4>
-            </Center>
-          </InputField>
-        )}
-        <Center>
-          <Button secondary type="submit" disabled={isSubmitting}>
-            Submit
-          </Button>
-        </Center>
-      </Form>
-    )}
-  </Formik>
+  <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
+    <input type="hidden" name="form-name" value="contact" />
+    <InputField>
+      <Input type="text" name="name" component="input" aria-label="name" placeholder="Full name*" />
+      {/* <ErrorMessage component={Error} name="name" /> */}
+    </InputField>
+    <InputField>
+      <Input id="email" aria-label="email" component="input" type="email" name="email" placeholder="Email*" />
+      {/* <ErrorMessage component={Error} name="email" /> */}
+    </InputField>
+    <InputField>
+      <Input aria-label="message" id="message" rows="8" type="textarea" name="message" placeholder="Message*" />
+      {/* <ErrorMessage component={Error} name="message" /> */}
+    </InputField>
+    <Center>
+      <Button secondary type="submit">
+        Submit
+      </Button>
+    </Center>
+  </form>
 );
