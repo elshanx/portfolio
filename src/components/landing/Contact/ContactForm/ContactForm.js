0c0df26fd18045e05344b1a1fe6ebbfefff069c6
elshanx <elshanxakbarov@gmail.com>
2020-07-25 21:33:37 +0400

    linked form

--- src/components/landing/Contact/ContactForm/ContactForm.js
+++ src/components/landing/Contact/ContactForm/ContactForm.js
@@ -1,28 +1,3 @@
-// import React from 'react';
-// import { Button, Input } from 'components/common';
-// import { Error, Center, InputField } from './styles';
-
-// const IndexPage = () => (
-//   <form name="contact-form" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
-//     <InputField>
-//       <Input name="name" placeholder="Your Name" type="text" />
-//     </InputField>
-//     <InputField>
-//       <Input name="email" placeholder="name@name.com" type="email" />
-//     </InputField>
-//     <InputField>
-//       <Input aria-label="message" id="message" rows="15" type="textarea" name="message" placeholder="Message*" />
-//     </InputField>
-//     <Center>
-//       <Button secondary type="submit">
-//         Submit
-//       </Button>
-//     </Center>
-//   </form>
-// );
-
-// export default IndexPage;
-
 import React, { useState } from 'react';
 import { navigate } from 'gatsby-link';
 import { Button, Input } from 'components/common';
@@ -67,34 +42,45 @@ const ContactForm = () => {
     >
       <input type="hidden" name="form-name" value="contact" />
       <p hidden>
-        <label>
-          Don’t fill this out: <input name="bot-field" onChange={handleChange} />
-        </label>
+        <input name="bot-field" onChange={handleChange} />
       </p>
-      <p>
-        <label>
-          Your name:
-          <br />
-          <input type="text" name="name" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Your email:
-          <br />
-          <input type="email" name="email" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <label>
-          Message:
-          <br />
-          <textarea name="message" onChange={handleChange} />
-        </label>
-      </p>
-      <p>
-        <button type="submit">Send</button>
-      </p>
+      <InputField>
+        <Input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          component="input"
+          aria-label="name"
+          placeholder="Full name*"
+        />
+      </InputField>
+      <InputField>
+        <Input
+          placeholder="Email*"
+          type="email"
+          name="email"
+          onChange={handleChange}
+          id="email"
+          aria-label="email"
+          component="input"
+        />
+      </InputField>
+      <InputField>
+        <Input
+          aria-label="message"
+          id="message"
+          rows="8"
+          type="textarea"
+          name="message"
+          placeholder="Message*"
+          onChange={handleChange}
+        />
+      </InputField>
+      <Center>
+        <Button secondary type="submit">
+          Submit
+        </Button>
+      </Center>
     </form>
   );
 };
--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -3,7 +3,6 @@ import { Container } from 'components/common';
 import contact from 'assets/illustrations/contact.svg';
 import { Wrapper, Details, Thumbnail } from './styles';
 import ContactForm from './ContactForm/ContactForm.js';
-// link the form
 
 export const Contact = () => (
   <Wrapper as={Container} id="contact">
