dbdd79281c701532f192e74a9e2191026cb61abe
elshanx <elshanxakbarov@gmail.com>
2020-07-26 23:49:28 +0400

    modified url

--- src/data/config.js
+++ src/data/config.js
@@ -2,7 +2,7 @@ module.exports = {
   defaultTitle: 'Elshanx',
   logo: 'https://portfolio.smakosh.com/favicon/favicon-512.png',
   author: 'Elshan Akbarov',
-  url: 'https://github.com/elshanx',
+  url: 'https://elshanx.netlify.app',
   legalName: 'Elshan Akbarov',
   defaultDescription: 'I’m Elshan and I’m a Front-end developer!',
   socialLinks: {
