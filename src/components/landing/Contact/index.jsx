b7ae53052e740a2a4e7f358590f3c798a9f63a4c
elshanx <elshanxakbarov@gmail.com>
2020-08-16 14:27:05 +0400

    Yup validation schema

--- src/components/landing/Contact/index.jsx
+++ src/components/landing/Contact/index.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import { Container } from 'components/common';
 import contact from 'assets/illustrations/contact.svg';
 import { Wrapper, Details, Thumbnail } from './styles';
-import ContactForm from './ContactForm/ContactForm.js';
+import ContactForm from './ContactForm/ContactForm.jsx';
 
 export const Contact = () => (
-  <Wrapper as={Container} id="contact">
+  <Wrapper as={Container} id='contact'>
     <Details>
       <ContactForm />
     </Details>
     <Thumbnail>
-      <img src={contact} alt="I’m Elshan and I’m a Front-end developer" />
+      <img src={contact} alt='I’m Elshan and I’m a Front-end developer' />
     </Thumbnail>
   </Wrapper>
 );
